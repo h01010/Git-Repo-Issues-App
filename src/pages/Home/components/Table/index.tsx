8d0532ef0a54d7fc2f7d8a2a948920f74511f0b4
Yeonju Seo(서연주) <44168693+h01010@users.noreply.github.com>
2023-12-30 16:56:42 +0900

    feat: Home 하위 컴포넌트 분리
    - issue 구역으로 분리되는 컴포넌트들을 Home 하위 components로 분리
    - Filter, Table, Pagination 컴포넌트 파일 생성

---
+++ src/pages/Home/components/Table/index.tsx
@@ -0,0 +1,5 @@
+const Table = () => {
+  return <div className="issue__table">이슈 테이블</div>;
+};
+
+export default Table;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,18 @@
 import Title from '../../components/Title';
-import { HomeBox } from './style';
+import Filter from './components/Filter';
+import Pagination from './components/Pagination';
+import Table from './components/Table';
+import { HomeBox, IssueBox } from './style';
 
 function Home() {
   return (
     <HomeBox>
       <Title text="이슈정리" />
-      <div className="issue__list">
-        <div className="issue__filter">
-          <div>이슈 상태</div>
-          <div>sort 필터</div>
-        </div>
-        <div className="issue__table">이슈 테이블</div>
-        <div className="issue__pagination">12345</div>
-      </div>
+      <IssueBox>
+        <Filter />
+        <Table />
+        <Pagination />
+      </IssueBox>
     </HomeBox>
   );
 }
